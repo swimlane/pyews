22f2fe3e32a263af5402dd95e7884b27b7e6ee7b
Josh Rickard <josh.rickard@swimlane.com>
2019-03-29 16:40:47 -0500

    modified the setup script

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyews',
-    version='0.1.0',
+    version='0.0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A Python package to interact with both on-premises and Office 365 Exchange Web Services',
     long_description=open('README.md').read(),
-    install_requires=[],
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'lxml',
+        'pyyaml'
+    ],
+    keywords='ews exchange office365 email ediscovery+',
     url='',
     author='Josh Rickard',
     author_email='josh.rickard@swimlane.com'
