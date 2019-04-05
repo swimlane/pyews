544a4da15c3d03bae1dc2a3f8c7a621c8621314b
Josh Rickard <josh.rickard@swimlane.com>
2019-04-05 16:36:14 -0500

    Modified test script and work in progress for serviceconfiguration class

---
+++ bin/serviceconfiguration.py
@@ -0,0 +1,162 @@
+#import pyews.service.serviceendpoint
+#from .serviceendpoint import ServiceEndpoint
+#from pyews import userconfiguration as UC
+#from pyews.utils.exceptions import ObjectType, SoapResponseHasError
+from bs4 import BeautifulSoup
+import requests
+
+
+class ServiceConfiguration(object):
+
+    def __init__(self, username, password):
+        '''Child class of ServiceEndpoint that is used to resolve names based on the provided UserConfiguration object.  This class is used as an alternative to Autodiscover
+        since ResolveNames endpoint is a common endpoint across all versions of Microsoft Exchange & Office 365.
+        
+        Args:
+            userconfiguration (UserConfiguration): A UserConfiguration object created using the UserConfiguration class
+        
+        Raises:
+            ObjectType: An incorrect object type has been used
+        '''
+        self.SOAP_REQUEST_HEADER = {'content-type': 'text/xml; charset=UTF-8'}
+
+     #   self.userconfiguration = userconfiguration
+        self.username = username
+        self.password = password
+
+        self._response = ''
+        self.invoke()
+
+    @property
+    def raw_soap(self):
+        '''Returns the raw SOAP response
+        
+        Returns:
+            str: Raw SOAP XML response
+        '''
+        return self._raw_soap
+
+    @raw_soap.setter
+    def raw_soap(self, value):
+        '''Sets the raw soap and response from a SOAP request
+        
+        Args:
+            value (str): The response from a SOAP request
+        '''
+        self.response = value
+        self._raw_soap = value
+
+    @property
+    def response(self):
+        '''ResolveNames SOAP response
+        
+        Returns:
+            str: Returns the ResolveNames response
+        '''
+        return self._response
+
+    @response.setter
+    def response(self, value):
+        '''Creates and sets a response object
+
+        Args:
+            value (str): The raw response from a SOAP request
+        '''
+        self._response = value
+
+    def invoke(self):
+        '''Used to invoke an ResolveNames SOAP request
+        
+        Raises:
+            SoapResponseHasError: Raises an error when unable to parse a SOAP response
+        '''
+       # config = self.userconfiguration
+        soap_payload = self.soap()
+        r = requests.post(
+            'https://autodiscover-s.outlook.com/EWS/Exchange.asmx',
+            data=soap_payload, 
+            headers=self.SOAP_REQUEST_HEADER, 
+            auth=(self.username, self.password)
+        )
+        parsed_response = BeautifulSoup(r.content, 'xml')
+        print(parsed_response)
+        if parsed_response.find('ResponseCode').string == 'NoError':
+            self.raw_soap = parsed_response
+        else:
+            raise AttributeError('Unable to parse response from ResolveNames')
+
+    def soap(self):
+
+        return '''<?xml version="1.0" encoding="UTF-8"?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+   <s:Header>
+      <h:ServerVersionInfo xmlns:h="http://schemas.microsoft.com/exchange/services/2006/types" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" MajorBuildNumber="1750" MajorVersion="15" MinorBuildNumber="15" MinorVersion="20" Version="V2018_01_08" />
+   </s:Header>
+   <s:Body>
+      <GetServiceConfigurationResponse xmlns="http://schemas.microsoft.com/exchange/services/2006/messages" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ResponseClass="Success">
+         <ResponseCode>NoError</ResponseCode>
+         <ResponseMessages>
+            <ServiceConfigurationResponseMessageType ResponseClass="Success">
+               <ResponseCode>NoError</ResponseCode>
+               <m:MailTipsConfiguration xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages">
+                  <t:MailTipsEnabled xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">true</t:MailTipsEnabled>
+                  <t:MaxRecipientsPerGetMailTipsRequest xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">50</t:MaxRecipientsPerGetMailTipsRequest>
+                  <t:MaxMessageSize xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">36700160</t:MaxMessageSize>
+                  <t:LargeAudienceThreshold xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">25</t:LargeAudienceThreshold>
+                  <t:ShowExternalRecipientCount xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">false</t:ShowExternalRecipientCount>
+                  <t:InternalDomains xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">
+                     <t:Domain IncludeSubdomains="false" Name="swimlaneresearchdev.onmicrosoft.com" />
+                  </t:InternalDomains>
+                  <t:PolicyTipsEnabled xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">false</t:PolicyTipsEnabled>
+                  <t:LargeAudienceCap xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">1000</t:LargeAudienceCap>
+               </m:MailTipsConfiguration>
+            </ServiceConfigurationResponseMessageType>
+         </ResponseMessages>
+      </GetServiceConfigurationResponse>
+   </s:Body>
+</s:Envelope>'''
+
+
+        return '''<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages"
+               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"
+               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+               xmlns:xs="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <t:RequestServerVersion Version="Exchange2010" />
+  </soap:Header>
+  <soap:Body>
+    <m:GetUserConfiguration>
+      <m:UserConfigurationName Name="TestConfig">
+        <t:DistinguishedFolderId Id="drafts"/>
+      </m:UserConfigurationName>
+      <m:UserConfigurationProperties>Dictionary</m:UserConfigurationProperties>
+    </m:GetUserConfiguration>
+  </soap:Body>
+</soap:Envelope>'''
+
+        return '''<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+               xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages"
+               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"
+               xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
+               xmlns:xs="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <t:RequestServerVersion Version="Exchange2016" />
+  </soap:Header>
+  <soap:Body>
+    <m:GetServiceConfiguration>
+        <m:ActingAs>
+            <t:EmailAddress>someguy@swimlaneresearchdev.onmicrosoft.com</t:EmailAddress>
+            <t:RoutingType>SMTP</t:RoutingType>
+        </m:ActingAs>
+      <m:RequestedConfiguration>
+        <m:ConfigurationName>MailTips</m:ConfigurationName>
+      </m:RequestedConfiguration>
+    </m:GetServiceConfiguration>
+  </soap:Body>
+</soap:Envelope>'''
+
+
+print(ServiceConfiguration('hackathon@swimlaneresearchdev.onmicrosoft.com','TWdrMHQUr7qNe!'))
--- bin/test.py
+++ bin/test.py
@@ -1,114 +1,14 @@
 
-'''
-from bs4 import BeautifulSoup
-'''
-text ='''<?xml version="1.0" encoding="UTF-8"?>
-<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-               xmlns:t="http://schemas.microsoft.com/exchange/services/2006/types"
-               xmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages">
-   <soap:Header>
-      <t:RequestServerVersion Version="%s" />
-   </soap:Header>
-   <soap:Body >
-      <m:GetSearchableMailboxes>
-         <m:ExpandGroupMembership>true</m:ExpandGroupMembership>
-      </m:GetSearchableMailboxes>
-   </soap:Body>
-</soap:Envelope>''' % 'Exchange2016'
+from pyews import UserConfiguration
 
-
-
-impersonation = '''<t:ExchangeImpersonation xmlns:t='http://schemas.microsoft.com/exchange/services/2006/types'>
-      <t:ConnectingSID>
-        <t:PrincipalName />
-        <t:SID />
-        <t:PrimarySmtpAddress>josh.rickard@swimlane.com</t:PrimarySmtpAddress>
-        <t:SmtpAddress />
-      </t:ConnectingSID>
-    </t:ExchangeImpersonation>'''
-
-
-
-'''
-def set_impersonation(self, bs_data, principalName=None, SID=None, primarySmtpAddress=None, smtpAddress=None)
-    if principalName is not None:
-        return self._new_impersonation_header(bs_data, 'PrincipalName')
-    elif SID is not None:
-        return self._new_impersonation_header(bs_data, 'SID')
-    elif primarySmtpAddress is not None:
-        return self._new_impersonation_header(bs_data, 'PrimarySmtpAddress')
-    elif smtpAddress is not None:
-        return self._new_impersonation_header(bs_data, 'SmtpAddress')
-
-    
-def _new_impersonation_header(self, bs_data, method, value):
-    impersonation_header = bs_data.new_tag('ExchangeImpersonation', namespace='http://schemas.microsoft.com/exchange/services/2006/types', nsprefix='t')
-    connectingSID = bs_data.new_tag('ConnectingSID', namespace='http://schemas.microsoft.com/exchange/services/2006/types', nsprefix='t')
-    impersonation_method = bs_data.new_tag('%s' % method, namespace='http://schemas.microsoft.com/exchange/services/2006/types', nsprefix='t')
-
-    connectingSID.append(impersonation_method)
-    impersonation_header.append(connectingSID)
-    header = bs_data.find('soap:Header')
-    header.append(impersonation_header)
-
-    # setting new impersonation header value
-    bs_data.Envelope.Header.ExchangeImpersonation.ConnectingSID.method.string = value
-    return bs_data
-
-'''
-
-'''
-* UserConfiguration
-** Credentials (services need this)
-** Autodiscover ( or not) (services need this)
-** impersonation settings (or not) (services needs this)
-** Exchange version information (services needs this)
-
-* ServiceConfiguration
-** 
-
-# Design Pattern
-
-Credentials are needed to authenticate
-User configuration has credentials
-'''
-from pyews.configuration import UserConfiguration
-
-
-import pyews.service as SERVICE
-#from pyews import GetSearchableMailboxes
-#from pyews import GetInboxRules
-#from pyews import SearchMailboxes
-#from pyews import DeleteItem
-
-#creds = Credentials('hackathon@swimlaneresearchdev.onmicrosoft.com', 'TWdrMHQUr7qNe!')
-
-# Example of not using Autodiscover
-'''
-userconfig = UserConfiguration(
-   'hackathon@swimlaneresearchdev.onmicrosoft.com',
-   'TWdrMHQUr7qNe!',
-   )
-
-userconfig.update()
-
-
-print(userconfig.UserDisplayName)
-print(userconfig.UserDN)
-print(userconfig.UserDeploymentId)
-print(userconfig.CasVersion)
-print(userconfig.EwsSupportedSchemas)
-print(userconfig.InternalMailboxServer)
-print(userconfig.MailboxDN)
-print(userconfig.ExternalEwsUrl)
-'''
+# Example of using Autodiscover
 
 userconfig = UserConfiguration(
    'hackathon@swimlaneresearchdev.onmicrosoft.com',
    'TWdrMHQUr7qNe!',
-   exchangeVersion='Office365'
 )
 
+# you can print properties on the useconfig object if needed
 
 #print(userconfig.autodiscover)
 #print(userconfig.configuration)
@@ -118,6 +18,7 @@ userconfig = UserConfiguration(
 #print(userconfig.exchangeVersion)
 #print(userconfig.raw_soap)
 
+# Example of NOT using Autodiscover
 '''
 userconfig = UserConfiguration(
    'hackathon@swimlaneresearchdev.onmicrosoft.com',
@@ -130,67 +31,45 @@ userconfig = UserConfiguration(
 
 
 # get searchable mailboxes based on your accounts permissions
+
+from pyews import GetSearchableMailboxes
+
 referenceid_list = []
-for mailbox in SERVICE.GetSearchableMailboxes(userconfig).response:
+for mailbox in GetSearchableMailboxes(userconfig).response:
     referenceid_list.append(mailbox['ReferenceId'])
 
+#print(referenceid_list)
+
+from pyews import SearchMailboxes
 messages_found = []
-for search in SERVICE.SearchMailboxes('subject:account', userconfig, referenceid_list).response:
+for search in SearchMailboxes('subject:"OMG PHISHING EMAIL"', userconfig, referenceid_list).response:
     messages_found.append(search['MessageId'])
     # we can print the results first if we want
-    #print(search['Subject'])
-    #print(search['MessageId'])
-    #print(search['Sender'])
-    #print(search['ToRecipients'])
-    #print(search['CreatedTime'])
-    #print(search['ReceivedTime'])
+    print(search['Subject'])
+    print(search['MessageId'])
+    print(search['Sender'])
+    print(search['ToRecipients'])
+    print(search['CreatedTime'])
+    print(search['ReceivedTime'])
     #etc.
 
+#print(messages_found)
+
+
 # if we wanted to now delete a specific message then we would call the DeleteItem class
-deleted_message_response = SERVICE.DeleteItem(messages_found[2], userconfig).response
+
+#from pyews import DeleteItem
+#deleted_message_response = DeleteItem(messages_found[0], userconfig).response
 
 #print(deleted_message_response)
 
 # once you have the mailboxes "referenceIds" (example in SearchMailboxes below) you need to loop through them and provide either a single referenceid or a list of them
-#print(SearchMailboxes('subject:account', userconfig, '/o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=2ae3541cef834557aadf2fa434590af2-mailbox1').response)
-#print(GetInboxRules('hackathon@swimlaneresearchdev.onmicrosoft.com', userconfig).response)
-#(print(GetSearchableMailboxes(userconfig).response))
 
-#print(temp_userconfig.ewsUrl)
-#print(userconfig.update(temp_userconfig))
-#print(userconfig.configuration)
+#from pyews import SearchMailboxes
+#mailboxSearch = SearchMailboxes('subject:account', userconfig, '/o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=2ae3541cef834557aadf2fa434590af2-mailbox1').response
+#print(mailboxSearch)
 
+# We can get InboxRules as well
 
-# if a user chooses to not use Autodiscover, then they must provide a exchange_version and ews_url
-#userconfig = UserConfiguration(
-#   'hackathon@swimlaneresearchdev.onmicrosoft.com',
-#   'TWdrMHQUr7qNe!',
-#   autodiscover=False,
-#   ews_url='https://outlook.office365.com/EWS/Exchange.asmx'
-#)
-
-
-#UserConfiguration()
-
-'''
-UserConfiguration()
-
-UserConfiguration(
-   username,
-   password,
-   autodiscover=True,
-
-   creds = Credentials('hackathon@swimlaneresearchdev.onmicrosoft.com', 'TWdrMHQUr7qNe!')
-
-# We provide that object to the Autodiscover service endpoint, which provides ewsurl and exchangeVersion attributes
-# additonally, when using Autodiscover it will create a UserSettings object which has additional information used throughout
-autodiscover = Autodiscover(credentials=creds, exchangeVersion='Office365')
-print("External EWS Url: %s" % autodiscover.usersettings.ExternalEwsUrl)
-
-# as you can see the autodiscover.usersettings contains a CasVersion.  This is used to determine the exact ExchangeVersion needed for all SOAP request calls.
-exchVer = ExchangeVersion(autodiscover.usersettings.CasVersion)
-)
-
-
-ServiceConfiguration(userConfig).GetSearchableMailboxes()
-'''
+#from pyews import GetInboxRules
+#mailboxRules = GetInboxRules('hackathon@swimlaneresearchdev.onmicrosoft.com', userconfig).response
