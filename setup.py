ed9ce007c9191d3a17a9ff8293b679ba37599641
Josh Rickard <josh.rickard@swimlane.com>
2019-05-23 11:47:38 -0500

    bumping version

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ def parse_requirements(requirement_file):
 
 setup(
     name='py-ews',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A Python package to interact with both on-premises and Office 365 Exchange Web Services',
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     install_requires=parse_requirements('./requirements.txt'),
-    keywords='ews exchange office365 email ediscovery swimlane',
+    keywords=['ews', 'exchange', 'office365', 'email', 'ediscovery', 'swimlane'],
     url='https://github.com/swimlane/pyews',
     author='Swimlane',
-    author_email='info@swimlane.com'
+    author_email='info@swimlane.com',
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4'
 )
