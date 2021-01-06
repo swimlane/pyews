2a4aa360db528b386442e4bdecf816916435302a
joshswimlane <josh.rickard@swimlane.com>
2021-01-06 11:10:34 -0600

    refactored endpoint class

--- pyews/configuration/endpoint.py
+++ pyews/configuration/endpoint.py
@@ -1,48 +1,13 @@
 
-from pyews.utils.exchangeversion import ExchangeVersion
-from pyews.utils.exceptions import ExchangeVersionError
 
+class Endpoint:
 
-class Endpoint(object):
-
-    def __init__(self, exchangeVersion, domain=None):
+    def __init__(self, domain=None):
         self.domain = domain
-        self.exchangeVersion = exchangeVersion
-        self.endpoint = exchangeVersion
 
-    @property
-    def exchangeVersion(self):
-        return self._exchangeVersion
-
-    @exchangeVersion.setter
-    def exchangeVersion(self, value):
-        exchange_ver_list = []
-        if isinstance(value, list):
-            for ver in value:
-                if ExchangeVersion.valid_version(ver):
-                    exchange_ver_list.append(ver)
-        else:
-            exchange_ver_list.append(value)
-        self._exchangeVersion = exchange_ver_list
-
-    @property
-    def domain(self):
-        return self._domain
-
-    @domain.setter
-    def domain(self, value):
-        self._domain = value
-
-    @property
-    def endpoint(self):
-        return self._endpoint
-
-    @endpoint.setter
-    def endpoint(self, value):
-        endpoint_list = []
+    def get(self):
+        endpoint_list = ['https://outlook.office365.com/autodiscover/autodiscover.svc']
         if self.domain:
             endpoint_list.append("https://{}/autodiscover/autodiscover.svc".format(self.domain))
             endpoint_list.append("https://autodiscover.{}/autodiscover/autodiscover.svc".format(self.domain))
-
-        endpoint_list.append('https://outlook.office365.com/autodiscover/autodiscover.svc')
-        self._endpoint = endpoint_list
+        return endpoint_list
