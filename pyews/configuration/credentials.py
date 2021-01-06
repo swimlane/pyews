3ed2aa201118f8ac874aea68e2bfec97582a815e
joshswimlane <josh.rickard@swimlane.com>
2021-01-06 11:10:06 -0600

    Updated handling of credentials

--- pyews/configuration/credentials.py
+++ pyews/configuration/credentials.py
@@ -1,6 +1,7 @@
 import re
 
-class Credentials(object):
+
+class Credentials:
     '''Creates a credential object for communicating with EWS
         
         Example:
@@ -28,26 +29,9 @@ class Credentials(object):
         '''
 
     def __init__(self, email_address, password):
-        
         self.email_address = email_address
         self.password = password
         self.domain = self.email_address
-
-    @property
-    def email_address(self):
-        return self._email_address
-
-    @email_address.setter
-    def email_address(self, value):
-        self._email_address = value
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, value):
-        self._password = value
 
     @property
     def domain(self):
