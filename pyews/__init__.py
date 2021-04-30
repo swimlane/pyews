b8fabae66397034f971b89035fc99afb7ae3ad93
joshswimlane <josh.rickard@swimlane.com>
2021-04-29 21:41:53 -0500

    removed and added imports to main package

--- pyews/__init__.py
+++ pyews/__init__.py
@@ -1,16 +1,1 @@
-from pyews.utils.logger import setup_logging
-setup_logging()
-
-from .core import Core
-from .configuration.userconfiguration import UserConfiguration
-from .configuration.endpoint import Endpoint
-from .configuration.autodiscover import Autodiscover
-from .configuration.credentials import Credentials
-from .service.resolvenames import ResolveNames
-from .service.deleteitem import DeleteItem
-from .service.getinboxrules import GetInboxRules
-from .service.findhiddeninboxrules import FindHiddenInboxRules
-from .service.getsearchablemailboxes import GetSearchableMailboxes
-from .service.searchmailboxes import SearchMailboxes
-from .utils.exchangeversion import ExchangeVersion
-from .utils.exceptions import CredentialsError, IncorrectParameters, SoapConnectionError, SoapConnectionRefused, ExchangeVersionError, ObjectType, SearchScopeError, SoapAccessDeniedError, SoapResponseHasError, SoapResponseIsNoneError, DeleteTypeError, UserConfigurationError
+from .ews import EWS
