79eb65e5369e664fad76a72dcb09aa20be9b80c6
joshswimlane <josh.rickard@swimlane.com>
2021-05-25 15:57:52 -0500

    Refactored Authentication class

--- pyews/core/authentication.py
+++ pyews/core/authentication.py
@@ -2,85 +2,87 @@ from .exchangeversion import ExchangeVersion
 from .core import Core
 
 
-class classproperty(property):
-    def __get__(self, obj, objtype=None):
-        return super(classproperty, self).__get__(objtype)
-    def __set__(self, obj, value):
-        super(classproperty, self).__set__(type(obj), value)
-    def __delete__(self, obj):
-        super(classproperty, self).__delete__(type(obj))
+class AuthenticationProperties(type):
 
+    def __set_initial_property_values(cls):
+        if isinstance(cls._credentials, tuple):
+            cls.domain = cls._credentials[0]
+            cls.endpoints = None
+            cls.exchange_versions = None
 
-class Authentication(Core):
-
-    def __init__(cls, username, password, ews_url=None, exchange_version=ExchangeVersion.EXCHANGE_VERSIONS, impersonate_as=None):
-        cls.impersonate_as = impersonate_as
-        cls.exchange_versions = exchange_version
-        cls.credentials = (username, password)
-        cls.domain = username
-        cls.endpoints = ews_url
-
-    @classproperty
+    @property
     def impersonate_as(cls):
-        return cls.__impersonate_as
-
+        if not cls._impersonate_as:
+            cls._impersonate_as = ''
+        return cls._impersonate_as
+    
     @impersonate_as.setter
     def impersonate_as(cls, value):
         if not value:
-            cls.__impersonate_as = ''
+            cls._impersonate_as = ''
         else:
-            cls.__impersonate_as = value
+            cls._impersonate_as = value
 
-    @classproperty
+    @property
     def credentials(cls):
         return cls._credentials
 
     @credentials.setter
     def credentials(cls, value):
         if isinstance(value, tuple):
-            cls.domain = value[0]
             cls._credentials = value
+            cls.__set_initial_property_values()
         else:
             raise AttributeError('Please provide both a username and password')
 
-    @classproperty
+    @property
     def exchange_versions(cls):
         return cls._exchange_versions
 
     @exchange_versions.setter
     def exchange_versions(cls, value):
-        from .exchangeversion import ExchangeVersion
         if not value:
-            cls._exchange_versions = ExchangeVersion.EXCHANGE_VERSIONS
-        elif not isinstance(value, list):
-            cls._exchange_versions = [value]
-        else:
-            cls._exchange_versions = value
+            from .exchangeversion import ExchangeVersion
+            value = ExchangeVersion.EXCHANGE_VERSIONS
+        if not isinstance(value, list):
+            value = [value]
+        cls._exchange_versions = value
 
-    @classproperty
+    @property
     def endpoints(cls):
         return cls._endpoints
 
     @endpoints.setter
     def endpoints(cls, value):
-        from .endpoints import Endpoints
         if not value:
-            cls._endpoints = Endpoints(cls.domain).get()
+            from .endpoints import Endpoints
+            cls._endpoints = Endpoints(cls._domain).get()
         elif not isinstance(value, list):
             cls._endpoints = [value]
         else:
             cls._endpoints = value
 
-    @classproperty
+    @property
     def domain(cls):
         return cls._domain
 
     @domain.setter
     def domain(cls, value):
-        '''Splits the domain from an email address
-        
-        Returns:
-            str: Returns the split domain from an email address
-        '''
-        local, _, domain = value.partition('@')
-        cls._domain = domain
+        temp_val = None
+        if '@' in value:
+            local, _, domain = value.partition('@')
+            temp_val = domain
+        elif value:
+            temp_val = value
+        else:
+            temp_val = None
+        cls._domain = temp_val
+
+
+class Authentication(object, metaclass=AuthenticationProperties):
+
+    _impersonate_as = None
+    _credentials = tuple()
+    _exchange_versions = []
+    _endpoints = []
+    _domain = None
