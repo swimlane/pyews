b2f86ea6d7563d41feccebdbe30f0ad8c81bdda3
joshswimlane <josh.rickard@swimlane.com>
2021-06-15 10:01:29 -0500

    Fixing imports in init

--- pyews/core/__init__.py
+++ pyews/core/__init__.py
@@ -2,4 +2,4 @@ from .authentication import Authentication
 from .core import Core
 from .endpoints import Endpoints
 from .exchangeversion import ExchangeVersion
-from .oauthconnector import OAuth2Connector
+from .oauth2connector import OAuth2Connector
